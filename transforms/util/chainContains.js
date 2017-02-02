d2e83c84357db7384c8e652ff24cf7d808b48e9c
Daniel Stockman <daniels@zillowgroup.com>
2017-02-02 11:46:02 -0800

    polish: reformat utils for readability

--- transforms/util/chainContains.js
+++ transforms/util/chainContains.js
@@ -1,12 +1,22 @@
 module.exports = j => (fnName, node, end) => {
   let curr = node;
-  const checkEnd = (typeof end === 'function') ? end : name => name === end;
 
-  while (curr.type === j.MemberExpression.name
-  && curr.property.name !== fnName
-  && !checkEnd(curr.property.name)) {
+  const equalsEnd = (
+    typeof end === 'function'
+      ? end
+      : name => name === end
+  );
+
+  while (
+    curr.type === j.MemberExpression.name &&
+    curr.property.name !== fnName &&
+    !equalsEnd(curr.property.name)
+  ) {
     curr = curr.object;
   }
 
-  return curr.type === j.MemberExpression.name && curr.property.name === fnName;
+  return (
+    curr.type === j.MemberExpression.name &&
+    curr.property.name === fnName
+  );
 };
